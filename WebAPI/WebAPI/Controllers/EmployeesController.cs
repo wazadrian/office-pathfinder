77d424b34fd892eb346e185c7331e53281c77202
Mateusz Janiszewski <mateusz2716@gmail.com>
2018-04-16 22:26:55 +0200

    CRUD operations v 1.1

--- WebAPI/WebAPI/Controllers/EmployeesController.cs
+++ WebAPI/WebAPI/Controllers/EmployeesController.cs
@@ -14,22 +14,16 @@ namespace WebAPI.Controllers
     [Route("api/[controller]")]
     public class EmployeesController : Controller
     {
-        //private const string _endpointUri = "https://officepathfinder.documents.azure.com:443/";
-        //private const string _primaryKey = "WPNnm5XPIEcw5INS2Pxb3iNJqJIpNGJ0jqlb0jH7Nn1fgyEcvLTwEI0NCp2MjuPke2MACQKi0FonFnlvoeSCyg==";
-        //private DocumentClient _client = new DocumentClient(new Uri(_endpointUri), _primaryKey);
-
-        public EmployeesController()
-        {
-
-        }
-
+        private string databaseName = "Office";
+        private string collectionName = "EmployeesCollection";
+   
         [HttpGet]
         public List<Employee> Get()
         {
             FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
 
             var employeesQuery = CosmosDbConnection.client.CreateDocumentQuery<Employee>(
-                UriFactory.CreateDocumentCollectionUri("Office", "EmployeesCollection"), queryOptions);
+                UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), queryOptions);
 
             var employees = new List<Employee>(employeesQuery);
 
@@ -38,10 +32,8 @@ namespace WebAPI.Controllers
 
         [HttpPost]
         public async void CreateEmployee([FromBody] Employee employee)
-        {
-            
-            await CreateDocumentIfNotExists("Office", "EmployeesCollection", employee);
-
+        {   
+            await CreateDocumentIfNotExists(databaseName, collectionName, employee);
         }
 
         private async Task CreateDocumentIfNotExists(string databaseName, string collectionName, Employee employee)
@@ -64,14 +56,12 @@ namespace WebAPI.Controllers
             }
         }
 
-        
+
 
         [HttpDelete("{id}")]
         public async void DeleteEmployeeAsync(string id)
         {
-            //DeleteEmployeeDocument("Office", "EmployeesCollection", id);
-            await DeleteEmployeeDocument("Office", "EmployeesCollection", id);
-
+            await DeleteEmployeeDocument(databaseName, collectionName, id);
         }
 
         private async Task DeleteEmployeeDocument(string databaseName, string collectionName, string documentId)
@@ -83,16 +73,12 @@ namespace WebAPI.Controllers
         public async void UpdateEmployeeAsync(string id, [FromBody] Employee employee)
         {
             employee.id = id;
-            await UpdateDocument("Office", "EmployeesCollection", id, employee);
+            await UpdateDocument(databaseName, collectionName, id, employee);
         }
 
         private async Task UpdateDocument(string databaseName, string collectionName, string documentId, Employee updatedEmployee)
         {
             await CosmosDbConnection.client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, documentId), updatedEmployee);
         }
-
-
-
-
     }
 }
--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -16,37 +16,10 @@ namespace WebAPI
 {
     public class Program
     {
-       
 
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-
-            try
-            {
-                Program p = new Program();
-                p.GetStartedDemo().Wait();
-            }
-            catch (DocumentClientException de)
-            {
-                Exception baseException = de.GetBaseException();
-                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
-            }
-            catch (Exception e)
-            {
-                Exception baseException = e.GetBaseException();
-                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
-            }
-            finally
-            {
-                Console.WriteLine("End of demo, press any key to exit.");
-                Console.ReadKey();
-            }
-        }
-
-        private async Task GetStartedDemo()
-        {
-
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
