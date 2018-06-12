bc74e738a17b92050b14ed80942b7d4cdf0f42fd
Adrian Wąż <226042@student.pwr.edu.pl>
2018-06-12 22:02:18 +0200

    searchLayout

--- MobileBackend/PathFinder/MainActivity.cs
+++ MobileBackend/PathFinder/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Views;
 using System.Collections.Generic;
+using MobileBackend.Models;
+using PathFinder.Data;
 
 
 namespace PathFinder
@@ -11,35 +13,46 @@ namespace PathFinder
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            IDatabaseConnection databaseConnection = new MockDatabase();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
             var imageView = FindViewById<ImageView>(Resource.Id.mainImageView);
+            imageView.SetImageResource(Resource.Drawable.ppg);
+
             var buttonMap = FindViewById<Button>(Resource.Id.mapButton);
             var buttonSearch = FindViewById<Button>(Resource.Id.searchButton);
             var buttonExit = FindViewById<Button>(Resource.Id.exitButton);
             var inputText = FindViewById<EditText>(Resource.Id.inputText);
-            var outputListView = FindViewById<ListView>(Resource.Id.outputListView);
 
-            //_searchingEngine = new SearchingEngine();
-            //imageView.SetImageResource(Resource.Drawable.ppg);
+
+
+            var homeButton = FindViewById<Button>(Resource.Id.homeButton);
+            //searchLayout
+            var searchLayoutList = FindViewById<ListView>(Resource.Id.searchLayoutList);
+            var searchLayoutTextBox = FindViewById<EditText>(Resource.Id.searchLayoutTextBox);
+            var searchLayoutSearchButton = FindViewById<Button>(Resource.Id.searchLayoutSearchButton);
+            // /searchLayout
+
+
 
             buttonSearch.Click += (e, o) =>
-              {
-                  string dataToSearch = inputText.Text;
-                  
-                  //listBoxEmployees.Items.Clear();
-                 // _employees = _searchingEngine.FindEmployees("Sa");
-                //  foreach (var emp in _employees)
-                 // {
-                      //outputListView.SetAdapter(_employees);// Item .Add($"{emp.employeeName} {emp.employeeSurname}");
-                //  }
-              };
+            {
+                SetContentView(Resource.Layout.searchLayout);
+            };
+
+            /*homeButton.Click += (e, o) =>
+            {
+                SetContentView(Resource.Layout.activity_main);
+            };*/
+
+
+
         }
     }
 }
--- MobileBackend/PathFinder/Resources/Resource.Designer.cs
+++ MobileBackend/PathFinder/Resources/Resource.Designer.cs
@@ -2336,9 +2336,6 @@ namespace PathFinder
 			// aapt resource value: 0x7f080034
 			public const int FUNCTION = 2131230772;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int JakisButton = 2131230902;
-			
 			// aapt resource value: 0x7f080035
 			public const int META = 2131230773;
 			
@@ -2348,8 +2345,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800a5
-			public const int action0 = 2131230885;
+			// aapt resource value: 0x7f0800a2
+			public const int action0 = 2131230882;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2372,17 +2369,17 @@ namespace PathFinder
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f0800a2
-			public const int action_container = 2131230882;
+			// aapt resource value: 0x7f08009f
+			public const int action_container = 2131230879;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int action_divider = 2131230889;
+			// aapt resource value: 0x7f0800a6
+			public const int action_divider = 2131230886;
 			
-			// aapt resource value: 0x7f0800a3
-			public const int action_image = 2131230883;
+			// aapt resource value: 0x7f0800a0
+			public const int action_image = 2131230880;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2399,11 +2396,11 @@ namespace PathFinder
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800a4
-			public const int action_text = 2131230884;
+			// aapt resource value: 0x7f0800a1
+			public const int action_text = 2131230881;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int actions = 2131230898;
+			// aapt resource value: 0x7f0800af
+			public const int actions = 2131230895;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2438,8 +2435,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int cancel_action = 2131230886;
+			// aapt resource value: 0x7f0800a3
+			public const int cancel_action = 2131230883;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2453,8 +2450,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int chronometer = 2131230894;
+			// aapt resource value: 0x7f0800ab
+			public const int chronometer = 2131230891;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2465,14 +2462,14 @@ namespace PathFinder
 			// aapt resource value: 0x7f080039
 			public const int collapseActionView = 2131230777;
 			
-			// aapt resource value: 0x7f080096
-			public const int container = 2131230870;
+			// aapt resource value: 0x7f080093
+			public const int container = 2131230867;
 			
 			// aapt resource value: 0x7f080066
 			public const int contentPanel = 2131230822;
 			
-			// aapt resource value: 0x7f080097
-			public const int coordinator = 2131230871;
+			// aapt resource value: 0x7f080094
+			public const int coordinator = 2131230868;
 			
 			// aapt resource value: 0x7f08006d
 			public const int custom = 2131230829;
@@ -2486,23 +2483,20 @@ namespace PathFinder
 			// aapt resource value: 0x7f080060
 			public const int default_activity_button = 2131230816;
 			
-			// aapt resource value: 0x7f08008d
-			public const int descriptionTextView = 2131230861;
-			
-			// aapt resource value: 0x7f080099
-			public const int design_bottom_sheet = 2131230873;
-			
-			// aapt resource value: 0x7f0800a0
-			public const int design_menu_item_action_area = 2131230880;
-			
-			// aapt resource value: 0x7f08009f
-			public const int design_menu_item_action_area_stub = 2131230879;
-			
-			// aapt resource value: 0x7f08009e
-			public const int design_menu_item_text = 2131230878;
+			// aapt resource value: 0x7f080096
+			public const int design_bottom_sheet = 2131230870;
 			
 			// aapt resource value: 0x7f08009d
-			public const int design_navigation_view = 2131230877;
+			public const int design_menu_item_action_area = 2131230877;
+			
+			// aapt resource value: 0x7f08009c
+			public const int design_menu_item_action_area_stub = 2131230876;
+			
+			// aapt resource value: 0x7f08009b
+			public const int design_menu_item_text = 2131230875;
+			
+			// aapt resource value: 0x7f08009a
+			public const int design_navigation_view = 2131230874;
 			
 			// aapt resource value: 0x7f080020
 			public const int disableHome = 2131230752;
@@ -2513,8 +2507,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int end_padder = 2131230900;
+			// aapt resource value: 0x7f0800b1
+			public const int end_padder = 2131230897;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2558,11 +2552,14 @@ namespace PathFinder
 			// aapt resource value: 0x7f080021
 			public const int homeAsUp = 2131230753;
 			
+			// aapt resource value: 0x7f0800b2
+			public const int homeButton = 2131230898;
+			
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int icon_group = 2131230899;
+			// aapt resource value: 0x7f0800b0
+			public const int icon_group = 2131230896;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2570,11 +2567,11 @@ namespace PathFinder
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f0800af
-			public const int info = 2131230895;
+			// aapt resource value: 0x7f0800ac
+			public const int info = 2131230892;
 			
-			// aapt resource value: 0x7f080092
-			public const int inputText = 2131230866;
+			// aapt resource value: 0x7f08008c
+			public const int inputText = 2131230860;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2582,8 +2579,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
-			// aapt resource value: 0x7f080095
-			public const int largeLabel = 2131230869;
+			// aapt resource value: 0x7f080092
+			public const int largeLabel = 2131230866;
 			
 			// aapt resource value: 0x7f080049
 			public const int left = 2131230793;
@@ -2606,14 +2603,14 @@ namespace PathFinder
 			// aapt resource value: 0x7f08008f
 			public const int mapButton = 2131230863;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int masked = 2131230905;
+			// aapt resource value: 0x7f0800b8
+			public const int masked = 2131230904;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int media_actions = 2131230888;
+			// aapt resource value: 0x7f0800a5
+			public const int media_actions = 2131230885;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int message = 2131230903;
+			// aapt resource value: 0x7f0800b6
+			public const int message = 2131230902;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2624,14 +2621,11 @@ namespace PathFinder
 			// aapt resource value: 0x7f080028
 			public const int multiply = 2131230760;
 			
-			// aapt resource value: 0x7f08009c
-			public const int navigation_header_container = 2131230876;
+			// aapt resource value: 0x7f080099
+			public const int navigation_header_container = 2131230873;
 			
 			// aapt resource value: 0x7f08003b
 			public const int never = 2131230779;
-			
-			// aapt resource value: 0x7f0800b5
-			public const int niewiemTextView = 2131230901;
 			
 			// aapt resource value: 0x7f080022
 			public const int none = 2131230754;
@@ -2639,17 +2633,14 @@ namespace PathFinder
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int notification_background = 2131230897;
+			// aapt resource value: 0x7f0800ae
+			public const int notification_background = 2131230894;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int notification_main_column = 2131230891;
+			// aapt resource value: 0x7f0800a8
+			public const int notification_main_column = 2131230888;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int notification_main_column_container = 2131230890;
-			
-			// aapt resource value: 0x7f080093
-			public const int outputListView = 2131230867;
+			// aapt resource value: 0x7f0800a7
+			public const int notification_main_column_container = 2131230887;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2678,11 +2669,11 @@ namespace PathFinder
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int right_icon = 2131230896;
+			// aapt resource value: 0x7f0800ad
+			public const int right_icon = 2131230893;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int right_side = 2131230892;
+			// aapt resource value: 0x7f0800a9
+			public const int right_side = 2131230889;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2711,8 +2702,17 @@ namespace PathFinder
 			// aapt resource value: 0x7f080055
 			public const int scrollable = 2131230805;
 			
-			// aapt resource value: 0x7f080091
-			public const int searchButton = 2131230865;
+			// aapt resource value: 0x7f08008d
+			public const int searchButton = 2131230861;
+			
+			// aapt resource value: 0x7f0800b5
+			public const int searchLayoutList = 2131230901;
+			
+			// aapt resource value: 0x7f0800b4
+			public const int searchLayoutSearchButton = 2131230900;
+			
+			// aapt resource value: 0x7f0800b3
+			public const int searchLayoutTextBox = 2131230899;
 			
 			// aapt resource value: 0x7f080080
 			public const int search_badge = 2131230848;
@@ -2759,14 +2759,14 @@ namespace PathFinder
 			// aapt resource value: 0x7f080025
 			public const int showTitle = 2131230757;
 			
-			// aapt resource value: 0x7f080094
-			public const int smallLabel = 2131230868;
+			// aapt resource value: 0x7f080091
+			public const int smallLabel = 2131230865;
 			
-			// aapt resource value: 0x7f08009b
-			public const int snackbar_action = 2131230875;
+			// aapt resource value: 0x7f080098
+			public const int snackbar_action = 2131230872;
 			
-			// aapt resource value: 0x7f08009a
-			public const int snackbar_text = 2131230874;
+			// aapt resource value: 0x7f080097
+			public const int snackbar_text = 2131230871;
 			
 			// aapt resource value: 0x7f080043
 			public const int snap = 2131230787;
@@ -2789,8 +2789,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int status_bar_latest_event_content = 2131230887;
+			// aapt resource value: 0x7f0800a4
+			public const int status_bar_latest_event_content = 2131230884;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2816,8 +2816,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080069
 			public const int textSpacerNoTitle = 2131230825;
 			
-			// aapt resource value: 0x7f0800a1
-			public const int text_input_password_toggle = 2131230881;
+			// aapt resource value: 0x7f08009e
+			public const int text_input_password_toggle = 2131230878;
 			
 			// aapt resource value: 0x7f080014
 			public const int textinput_counter = 2131230740;
@@ -2825,8 +2825,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int time = 2131230893;
+			// aapt resource value: 0x7f0800aa
+			public const int time = 2131230890;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2843,8 +2843,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f08006e
 			public const int topPanel = 2131230830;
 			
-			// aapt resource value: 0x7f080098
-			public const int touch_outside = 2131230872;
+			// aapt resource value: 0x7f080095
+			public const int touch_outside = 2131230869;
 			
 			// aapt resource value: 0x7f08000f
 			public const int transition_current_scene = 2131230735;
@@ -2873,11 +2873,8 @@ namespace PathFinder
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b8
-			public const int visible = 2131230904;
-			
-			// aapt resource value: 0x7f08008c
-			public const int welcomeTextView = 2131230860;
+			// aapt resource value: 0x7f0800b7
+			public const int visible = 2131230903;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
